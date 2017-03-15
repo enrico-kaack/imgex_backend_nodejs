c133d8cb8f625c073db655f7978f4f790bb2ef46
enrico-kaack <e.kaack@live.de>
2017-03-15 20:14:50 +0100

    implemented: doesUserExist, groups/:groupId/addMEmber

--- app/routes/groups.js
+++ app/routes/groups.js
@@ -3,6 +3,7 @@ var Group = require.main.require('./app/models/group')
 var Device = require.main.require('./app/models/device')
 var MediaContent = require.main.require('./app/models/mediaContent')
 
+
 module.exports = function (mongoose, app, apiRoutes) {
     apiRoutes.get('/groups/', function (req, res) {
 
@@ -21,57 +22,69 @@ module.exports = function (mongoose, app, apiRoutes) {
 
         });
         group.save(function (err) {
-            if (err){
+            if (err) {
                 res.json({success: false, message: "Error saving new group"})
-            }else{
+            } else {
                 res.json({success: true, group: group});
             }
         })
     });
 
     apiRoutes.put('/groups/:groupId', function (req, res) {
-      isUserAdminOfGroup(req.decoded.user_id, req.params.groupId, function (admin) {
-          if (admin){
-          Group.findByIdAndUpdate(req.params.groupId, req.body.group, {new: true}, function (err, doc) {
-              if (err) {
-                  res.send(500, { success: false, message: "Group not found" });
-              }
-              else {
-                  res.json({success: true, group: doc});
-              }
-          });
-      }else{
-            res.status(403).send({success: false, message: "User is not admin!"});
-        }
-      });
-
-
-
+        isUserAdminOfGroup(req.decoded.user_id, req.params.groupId, function (admin) {
+            if (admin) {
+                Group.findByIdAndUpdate(req.params.groupId, req.body.group, {new: true}, function (err, doc) {
+                    if (err) {
+                        res.send(500, {success: false, message: "Group not found"});
+                    }
+                    else {
+                        res.json({success: true, group: doc});
+                    }
+                });
+            } else {
+                res.status(403).send({success: false, message: "User is not admin!"});
+            }
+        });
 
 
     })
 
-    apiRoutes.put('/groups/:groupId/addMembers', function (req, res) {
-        //tbd: check if user is admin of the group
-        //tbd: check if requested user exist
-        //tbd: not working for multiple users
+    apiRoutes.put('/groups/:groupId/addMember', function (req, res) {
+        isUserAdminOfGroup(req.decoded.user_id, req.params.groupId, function (isUserAdmin) {
+            if (!isUserAdmin){
+                res.status(500).send({success: false, message: "User is no admin of group"});
+            }else {
+                doesUsersExist(req.body.members._id, function (userExist) {
+                    if (userExist) {
+                        Group
+                            .findByIdAndUpdate(
+                                req.params.groupId,
+                                {
+                                    $push: {
+                                        "members": {
+                                            _id: req.body.members._id, admin: false
+                                        }
+                                    }
+                                },
+                                {upsert: true},
+                                function (err, model) {
+                                    if (err){
+                                        res.status(500).send({success: false, message: "Adding member failed"});
+                                    }else{
+                                        res.send({success: true, group: model});
+                                    }
 
-        Group
-            .findByIdAndUpdate(
-            req.params.groupId,
-            {$push: {
-                "members": {
-                $each: [{_id: "bla1", admin: false},{_id: "dada", admin: false}]
-            }}},
-            {safe: true, upsert: true},
-            function(err, model) {
-                res.status(500).send({ success: false, message: "Adding member failed" });
-            });
+                                });
+                    } else {
+                        res.status(500).send({success: false, message: "User does not exist"});
+                    }
+                });
+            }
+
+        });
+
 
     })
-    
-
-
 }
 
 isUserAdminOfGroup = function (user_id, group_id, callback) {
@@ -80,11 +93,22 @@ isUserAdminOfGroup = function (user_id, group_id, callback) {
         .exec(function (err, group) {
             if (group.members.findIndex(function (element, index, array) {
                     return element._id === user_id && element.admin === true
-                }) != -1){
-                return callback(true);
-            }else{
-                return callback(false);
+                }) != -1) {
+                callback(true);
+            } else {
+                callback(false);
             }
 
         });
+}
+
+doesUsersExist = function (user_id, callback) {
+    User.
+        findById(user_id, function (err, user) {
+        if (err){
+            callback(false);
+        }else{
+            callback(true);
+        }
+    })
 }
